291dcf85b7f840a78af3c30e38e30c80c21d4539
clubPenguin420 <42702641+clubPenguin420@users.noreply.github.com>
2020-03-06 22:50:39 -0600

    Changed the import to be more efficient

--- src/gui.py
+++ src/gui.py
@@ -1,34 +1,34 @@
-from tkinter import *
+import tkinter as tk
 
 
 
 def initWindow():
-    signIn = Tk("ImageDB")
+    signIn = tk.Tk("ImageDB")
     signIn.geometry("1000x700")
     signIn.state('zoomed')
 
-    userNameLabel = Label(signIn, text = "Username")
+    userNameLabel = tk.Label(signIn, text = "Username")
     userNameLabel.pack()
     userNameLabel.place(height = 40, width = 80, x = 580, y = 330)
 
-    userNameInput = Entry(signIn, bd = 5)
+    userNameInput = tk.Entry(signIn, bd = 5)
     userNameInput.pack()
     userNameInput.place(height = 40, width = 200 ,x = 650, y = 330)
 
 
-    passWordLabel = Label(signIn, text = "Password")
+    passWordLabel = tk.Label(signIn, text = "Password")
     passWordLabel.pack()
     passWordLabel.place(height = 40, width = 80, x = 580, y = 380)
 
-    passWordInput = Entry(signIn, bd = 5)
+    passWordInput = tk.Entry(signIn, bd = 5)
     passWordInput.pack()
     passWordInput.place(height = 40, width = 200 ,x = 650, y = 380)
 
-    logInBtn = Button(signIn, text = "Sign In")
+    logInBtn = tk.Button(signIn, text = "Sign In")
     logInBtn.pack()
     logInBtn.place(height = 30, width = 200, x = 650, y = 430)
 
-    createAccountBtn = Button(signIn, text = "Create an Account")
+    createAccountBtn = tk.Button(signIn, text = "Create an Account")
     createAccountBtn.pack()
     createAccountBtn.place(height = 30, width = 200, x = 650, y = 470)
 
