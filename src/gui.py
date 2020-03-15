26e3492f8dfea6f4fde4ddb40867077b071dc48e
clubPenguin420 <42702641+clubPenguin420@users.noreply.github.com>
2020-03-15 15:53:36 -0500

    Make the Create Account Gui

--- src/gui.py
+++ src/gui.py
@@ -3,7 +3,7 @@ import tkinter as tk
 
 
 def initSignInWindow():
-    signIn = tk.Tk("ImageDB")
+    signIn = tk.Tk("SignIn")
     signIn.geometry("1000x700")
     signIn.state('zoomed')
 
@@ -34,5 +34,38 @@ def initSignInWindow():
 
     signIn.mainloop()
 
+def initCreateAccountWindow():
+    create = tk.Tk("Create your account")
+    create.geometry("1000x700")
+    create.state('zoomed')    
 
-initSignInWindow()
+    userNameLabel = tk.Label(create, text = "Username")
+    userNameLabel.pack()
+    userNameLabel.place(height = 40, width = 80, x = 580, y = 330)
+
+    userNameInput = tk.Entry(create, bd = 5)
+    userNameInput.pack()
+    userNameInput.place(height = 40, width = 200 ,x = 650, y = 330)
+
+
+    passWordLabel = tk.Label(create, text = "Password")
+    passWordLabel.pack()
+    passWordLabel.place(height = 40, width = 80, x = 580, y = 380)
+
+    passWordInput = tk.Entry(create, bd = 5)
+    passWordInput.pack()
+    passWordInput.place(height = 40, width = 200 ,x = 650, y = 380)
+
+    logInBtn = tk.Button(create, text = "Create Account and Logon")
+    logInBtn.pack()
+    logInBtn.place(height = 30, width = 200, x = 650, y = 430)
+
+    exitBtn = tk.Button(create, text = "Exit to Sign In screen")
+    exitBtn.pack()
+    exitBtn.place(height = 30, width = 200, x = 650, y = 460)
+
+
+    create.mainloop()
+
+
+initCreateAccountWindow()
